e824fe82526b29462f51ead55a64a6ce46a93130
sandraghaoui <sfg04@mail.aub.edu>
2024-11-27 19:08:52 +0200

    Main method

---
+++ Main.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+srand(time(NULL));
+char difficulty[5];
+char easy[]="easy";
+char hard[]="hard";
+printf("\nLet's play! Enter the difficulty level (easy/hard): ");
+scanf("%s", &difficulty);
+getchar();
+int difficult;
+if(strcmp(difficulty, easy)==0){
+    difficult=0;
+}
+else difficult=1;
+int numberplayers;
+printf("How many players are playing? (Enter 1 or 2)\n");
+scanf("%d", &numberplayers);
+if(numberplayers==2){
+    twoplayermode(difficult);
+}
+else{
+    int level;
+    printf("How smart do you want your enemy to be? (Level 1, 2, or 3)\n");
+    scanf("%d", &level);
+    if(level==1){
+        easy(difficult);
+    }
+    else if(level==2){
+        medium(difficult);
+    }
+    else{
+        hard(difficult);
+    }
+  
+}
+}
