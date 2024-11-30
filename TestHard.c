394ab0b8702ccb31c5672977dd636109adfcb47a
JeanGhor <jjrjgh@gmail.com>
2024-12-01 01:20:07 +0200

    Add files via upload

--- TestHard.c
+++ TestHard.c
@@ -582,13 +582,13 @@ int calculateProb(int** pTable,int** grid)
     {
         for(int j = 0; j < 10; j++)
         {
-            if(grid[i][j]==0||grid[i][j]==1||grid[i][j]==4)//if the case is untouched     //the method will go over each case of the matrix and everytime a case can posess a boat of size 
+            if(grid[i][j]==0||grid[i][j]==1||grid[i][j]==2||grid[i][j]==4)//if the case is untouched     //the method will go over each case of the matrix and everytime a case can posess a boat of size 
             {                                                                              // 2 to 5, we will increment the pTable position of said boat starting from the crrent square to 
             int obstructedV=0;                                                              //to the rest of the matrix. After the end of the method, the case possessing the biggest number
             int obstructedH=0;                                                              // has the highest probability of housing a boat.
             for(int boat = 1; boat < 5 && i+boat < 10; boat++)
                 {
-                    if( (grid[i+boat][j]==1||grid[i+boat][j]==0||grid[i+boat][j]==4)&&!obstructedH) //check if a boat can be hidding
+                    if( (grid[i+boat][j]==1||grid[i+boat][j]==0||grid[i][j+boat]==2||grid[i+boat][j]==4)&&!obstructedH) //check if a boat can be hidding
                     {
                         for(int z=0; z < boat + 1;z++)                                              
                             pTable[i+z][j]++; 
@@ -598,13 +598,23 @@ int calculateProb(int** pTable,int** grid)
                 }            
             for(int boat = 1; boat < 5&&j+boat<10;boat++)
                 {
-                    if( (grid[i][j+boat]==1||grid[i][j+boat]==0 || grid[i][j+boat]==4)&&!obstructedV)
+                    if( (grid[i][j+boat]==1||grid[i][j+boat]==0||grid[i][j+boat]==2 || grid[i][j+boat]==4)&&!obstructedV)
                     {
                         for(int z=0; z < boat + 1;z++)
                             pTable[i][j+z]++; 
                     }
                     else
                         obstructedV=0;
+                }
+            }
+        }
+        for(int i = 0 ; i<10;i++)
+        {
+            for(int j =0;j<10;j++)
+            {
+                if(grid[i][j]==2||grid[i][j]==3)
+                {
+                    pTable[i][j]=0;
                 }
             }
         }
