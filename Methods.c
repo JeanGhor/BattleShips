f63aa606e10bffd6493cb81bbe42cd9814528297
JeanGhor <jjrjgh@gmail.com>
2024-12-02 03:33:35 +0200

    Updated methods with Specs and testing strategy

--- Methods.c
+++ Methods.c
@@ -496,5 +496,243 @@ int hit_or_miss(int** grid, int** Boats){
     }
     return sunk;
 }
+//Requires: The pTable After its calculation with calculateProb, and a pointer
+//Effect: returns the index of the cell that contains the highest probability through the index pointer
+
+void highestProb(int** pTable, int* index){
+    int maxi = pTable[0][0];
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < 10; j++){
+            if(pTable[i][j] >= maxi){
+                maxi = pTable[i][j];
+                index[0] = i;
+                index[1] = j;
+                index[2] = maxi;            
+            }
+        }
+    }
+}
+//Requires: 2 grids, the boat grid of the ennemy(grid), and a grid to fill up(pTable)
+//Effects: Calculates how much boats can fit inside each cell of the pTable.
+//Testing strategy: start by trying to calculate the pTable of an empty grid, then add misses and hits to test it's normal behaviior
+int calculateProb(int** pTable,int** grid)
+{
+    for (int i = 0; i < 10; i++)
+    {
+        for(int j = 0; j < 10; j++)
+        {
+            if(grid[i][j]==0||grid[i][j]==1||grid[i][j]==2||grid[i][j]==4)//if the case is untouched     //the method will go over each case of the matrix and everytime a case can posess a boat of size 
+            {                                                                              // 2 to 5, we will increment the pTable position of said boat starting from the crrent square to 
+            int obstructedV=0;                                                              //to the rest of the matrix. After the end of the method, the case possessing the biggest number
+            int obstructedH=0;                                                              // has the highest probability of housing a boat.
+            for(int boat = 1; boat < 5 && i+boat < 10; boat++)
+                {
+                    if( (grid[i+boat][j]==1||grid[i+boat][j]==0||grid[i][j+boat]==2||grid[i+boat][j]==4)&&!obstructedH) //check if a boat can be hidding
+                    {
+                        for(int z=0; z < boat + 1;z++)                                              
+                            pTable[i+z][j]++; 
+                    }
+                    else                                                                             //if it can't be hidding, then a boat of superior sized can't be either
+                        obstructedH=0;
+                }            
+            for(int boat = 1; boat < 5&&j+boat<10;boat++)
+                {
+                    if( (grid[i][j+boat]==1||grid[i][j+boat]==0||grid[i][j+boat]==2 || grid[i][j+boat]==4)&&!obstructedV)
+                    {
+                        for(int z=0; z < boat + 1;z++)
+                            pTable[i][j+z]++; 
+                    }
+                    else
+                        obstructedV=0;
+                }
+            }
+        }
+        for(int i = 0 ; i<10;i++)
+        {
+            for(int j =0;j<10;j++)
+            {
+                if(grid[i][j]==2||grid[i][j]==3)
+                {
+                    pTable[i][j]=0;
+                }
+            }
+        }
+    }
+}
+//Requires: an integer between 0 and 7 and a 2D array
+//Effects: returns specific places to place boats through the templates array
+
+void SetUp(int x, int** templates){// !!!!!!!!this function's array should be plased in the main!!!!!!!!!!{
+    if (x == 0){
+        templates[0][0] = 0;
+        templates[0][1] = 0;
+        templates[0][2] = 1;
+        templates[1][0] = 2;
+        templates[1][1] = 2;
+        templates[1][2] = 0;
+        templates[2][0] = 6;
+        templates[2][1] = 4;
+        templates[2][2] = 1;
+        templates[3][0] = 9;
+        templates[3][1] = 7;
+        templates[3][2] = 0;   
+    }
+    else if (x == 1){
+        templates[0][0] = 1;
+        templates[0][1] = 1;
+        templates[0][2] = 0;
+        templates[1][0] = 3;
+        templates[1][1] = 3;
+        templates[1][2] = 1;
+        templates[2][0] = 0;
+        templates[2][1] = 8;
+        templates[2][2] = 1;
+        templates[3][0] = 8;
+        templates[3][1] = 2;
+        templates[3][2] = 1;   
+    }
+    else if (x == 2){
+        templates[0][0] = 7;
+        templates[0][1] = 0;
+        templates[0][2] = 0;
+        templates[1][0] = 4;
+        templates[1][1] = 3;
+        templates[1][2] = 0;
+        templates[2][0] = 0;
+        templates[2][1] = 4;
+        templates[2][2] = 1;
+        templates[3][0] = 9;
+        templates[3][1] = 1;
+        templates[3][2] = 0;   
+    }
+    else if (x == 3){
+        templates[0][0] = 0;
+        templates[0][1] = 5;
+        templates[0][2] = 1;
+        templates[1][0] = 6;
+        templates[1][1] = 2;
+        templates[1][2] = 0;
+        templates[2][0] = 1;
+        templates[2][1] = 6;
+        templates[2][2] = 1;
+        templates[3][0] = 5;
+        templates[3][1] = 0;
+        templates[3][2] = 1;   
+    }
+    else if (x == 4){
+        templates[0][0] = 3;
+        templates[0][1] = 1;
+        templates[0][2] = 0;
+        templates[1][0] = 8;
+        templates[1][1] = 5;
+        templates[1][2] = 0;
+        templates[2][0] = 4;
+        templates[2][1] = 7;
+        templates[2][2] = 0;
+        templates[3][0] = 0;
+        templates[3][1] = 2;
+        templates[3][2] = 1;   
+    }
+    else if (x == 5){
+        templates[0][0] = 1;
+        templates[0][1] = 8;
+        templates[0][2] = 1;
+        templates[1][0] = 5;
+        templates[1][1] = 2;
+        templates[1][2] = 1;
+        templates[2][0] = 9;
+        templates[2][1] = 3;
+        templates[2][2] = 0;
+        templates[3][0] = 0;
+        templates[3][1] = 0;
+        templates[3][2] = 1;   
+    }
+    else if (x == 6){
+        templates[0][0] = 8;
+        templates[0][1] = 0;
+        templates[0][2] = 0;
+        templates[1][0] = 2;
+        templates[1][1] = 6;
+        templates[1][2] = 0;
+        templates[2][0] = 0;
+        templates[2][1] = 3;
+        templates[2][2] = 1;
+        templates[3][0] = 9;
+        templates[3][1] = 8;
+        templates[3][2] = 0;   
+    }
+    else if (x == 7){
+        templates[0][0] = 0;
+        templates[0][1] = 1;
+        templates[0][2] = 0;
+        templates[1][0] = 6;
+        templates[1][1] = 4;
+        templates[1][2] = 1;
+        templates[2][0] = 4;
+        templates[2][1] = 2;
+        templates[2][2] = 1;
+        templates[3][0] = 7;
+        templates[3][1] = 7;
+        templates[3][2] = 0;   
+    }
+}
+//Requires: the parameters should include the length of the boat, the array that stores the robot's boats and its grid and the choosen template's boat position
+//Effects: positions the boat in the robot's grid in at predefined coordinates depending on the template
+//Testing Strategy: We do not need to consider the possibility of the int boat being abnormal as we specify it
+//in the caller method. Same principle for Boats and grid, they are the responsability of the main method.
+void robot_position_boat_hard(int boat, int**Boats, int**grid, int *chosen){
+    int col = chosen[0];
+    int row = chosen[1];
+    char direction = (chosen[2]==1) ? 'h' : 'v';
+    int x=0;
+    while (x == 0)
+    {
+        x = robot_position_boat_hard_helper(grid, col, row, direction, boat, Boats);
+    }   
+}
+//Requires: nothing
+//Effects: helps the robot_position_boat_hard by returning 0 if the location chosen is not available, and returns 1 if it is (after positioning the boat)
+//Testing Strategy: To test this helper function, we try out of bounds coordinates, coordinates overlaping with other boats, as well as valid coordinates.
+//Everything else is the responsability of the caller method.
+int robot_position_boat_hard_helper(int** grid, int col, int row, char direction, int boat, int** Boats){
+    if(direction=='h' || direction=='H'){
+        for(int i=0; i<boat; i++){
+            if(col+i>=10){     
+                return 0;
+            }
+        }
+        for(int i=0; i<boat; i++){
+            if(grid[row][col+i]==1){
+                return 0;
+            }
+        }
+        for(int i=0; i<boat; i++){
+            grid[row][col+i]=1;
+            Boats[boat - 2][i] = row * 10 + col + i;
+        }
+        return 1;
+    }
+    else{
+        for(int i=0; i<boat; i++){
+            if(row+i>=10){
+              return 0;   
+            }
+        }
+        for(int i=0; i<boat; i++)
+        {
+            if(grid[row+i][col]==1)
+            {
+                return 0;   
+            }
+        }
+        for(int i=0; i<boat; i++)
+        {
+            grid[row+i][col]=1;
+            Boats[boat - 2][i] = (row + i) * 10 + col;
+        }
+        return 1;
+    }
+}
+
 
 
